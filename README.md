6f5275381a2d55dfa94b63308f9d424bc843bb63
ankur jain <ajb98.itbhu@gmail.com>
2013-07-17 19:41:38 +0000

    google map canvas and basic search

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ journeyScrapbook
 ================
 
 my Attempt for my travel website
+-----------
+Importing the database:
+mysql -u root -p -h localhost journey < journey.sql
--- phpPages/user_main_page.php
+++ phpPages/user_main_page.php
@@ -9,6 +9,8 @@
 		<script type="text/javascript" src="../bootstrap/js/bootstrap.js"></script>
 		<link href="../bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">	
 		<link href="../bootstrap/css/tweaks.css" rel="stylesheet" type="text/css">
+<!--    <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyBqOSpe5NX2eude-qj1A0LMgyKs6-xWwys&sensor=false"></script>-->
+		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
 		<!--<script type="text/javascript" src="validate.js"></script>-->
 	</header>
 	<body>
@@ -27,32 +29,72 @@
 							</ul>
 							<form class="navbar-form pull-right" action="logout.php" method="post" name="login_form">
               					<input type="submit" class="btn" value="SignOut" />
-            				</form>
+            	</form>
 						</div>
 					</div>
 				</div>
 			</div>
-			<!--
-			<div class="bookshelf" style="background-image: url('../images/final.jpg');  height: 800px; ">
-				
-			</div>
-			-->
-			<div class="bookshelf" style="text-align: center">
-				<div class="book1" style="float: left">
-				<img src="../images/pad.png" />
-				</div >
-				<div class="book1" style="width:100px; height: 100px; float: left;">
+<script>
+function initialize() {
+  var map = new google.maps.Map(document.getElementById('googleMap'), {
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+  var defaultBounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(-33.8902, 151.1759),
+      new google.maps.LatLng(-33.8474, 151.2631));
+  map.fitBounds(defaultBounds);
+
+  var input = /** @type {HTMLInputElement} */(document.getElementById('target'));
+  var searchBox = new google.maps.places.SearchBox(input);
+  var markers = [];
+
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
+
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
+
+    map.fitBounds(bounds);
+  });
+
+  google.maps.event.addListener(map, 'bounds_changed', function() {
+    var bounds = map.getBounds();
+    searchBox.setBounds(bounds);
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+    </script>
+     		<div class="span12" id="searchPlace" style="height:60px;margin-left: 0px;">
+					<input id="target" type="text" placeholder="Search Box"/>
 				</div>
-				<div class="book1" style="float: left">
-				<img src="../images/pad.png" />
-				</div>
-				<div class="book1" style="width:100px; height: 100px; float: left;">
-				</div>
-				<div class="book1" style="float: left">
-				<img src="../images/pad.png" />
-				</div>
-				
-			</div>
+        <div class="span12" id="googleMap" style="height:600px;margin-left: 0px;">
+    		</div>
 		</div>
 	</body>
 </html>
